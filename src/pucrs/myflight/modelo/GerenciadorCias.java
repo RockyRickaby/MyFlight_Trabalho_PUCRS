44912e247e2de2c6784cbb92a42a02180147f957
Maurício Alcântara da Luz <iamthefreeman@outlook.com.br>
2023-05-08 16:44:49 -0300

    08/05, Mauricio - adicionado método "carregaDados()" em GerenciadorCias + o arquivo a ser lido

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -1,5 +1,12 @@
 package pucrs.myflight.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class GerenciadorCias {
@@ -32,5 +39,17 @@ public class GerenciadorCias {
 				return e;
 		}
 		return null;
+	}
+
+	public void carregaDados() throws IOException {
+		Path path = Paths.get("airlines.dat");
+		BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset());
+		String line = br.readLine();
+		while ((line = br.readLine()) != null) {
+			Scanner sc = new Scanner(line).useDelimiter(";");
+			String cod = sc.next(), nome = sc.next();
+			this.adicionar(new CiaAerea(cod, nome));
+			sc.close();
+		}
 	}
 }
